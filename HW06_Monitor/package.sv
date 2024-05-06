efc01936e303a6a59dcf9e7515ced1a86661024a
ndstung <104809107+ndstung@users.noreply.github.com>
2024-05-05 19:43:03 -0700

    Update package.sv

--- HW06_Monitor/package.sv
+++ HW06_Monitor/package.sv
@@ -1,17 +1,28 @@
-package hw06_monitor ; //user's package
+package project273; //project273 package
 
 import uvm_pkg::*; //necessary to include this package in user's package
 
+//include all sv files
 
-`include "sequence_item.sv"
-`include "sequence.sv"
-`include "sequencer.sv"
-`include "driver.sv"
-`include "monitor.sv"
-`include "reference.sv"
-`include "agent.sv"
-`include "scoreboard.sv"
-`include "environment.sv"
+`include "seq_item.sv"
+`include "complex_seq_item.sv"
+`include "complex_seq_item_time.sv"
+`include "complex_seq_item_fft.sv"
+`include "complex_seq_item_fre.sv"
+`include "seq_item_bit.sv"
+`include "seq_fixed_point.sv"
+`include "sequence1.sv"
+`include "sequencer1.sv"
+
+`include "driver1.sv"
+`include "monitor1.sv"
+`include "encoder1.sv"
+`include "ifft.sv"
+`include "fft.sv"
+`include "decoder1.sv"
+`include "scoreboard1.sv"
+`include "agent1.sv"
+`include "environment1.sv"
 `include "test.sv"
 
-endpackage : hw06_monitor
+endpackage : project273
